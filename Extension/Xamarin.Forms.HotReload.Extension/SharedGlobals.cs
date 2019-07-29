2ee3bb05f8edcdf9741d9bba364c3349dab16f70
Andrei Misiukevich <andrei.misiukevich@gmail.com>
2019-07-29 19:42:12 +0300

    bump extension to 1.4.0

--- Extension/Xamarin.Forms.HotReload.Extension/SharedGlobals.cs
+++ Extension/Xamarin.Forms.HotReload.Extension/SharedGlobals.cs
@@ -4,7 +4,7 @@ namespace Xamarin.Forms.HotReload.Extension
     {
         //Package defining constants.
         public const string PackageCollectionPath = "HotReloadExtension";
-        public const string AppVersion = "1.3.7";
+        public const string AppVersion = "1.4.0";
 
         //Settings store keys.
         public const string SavePreferencesPath = "SavePreferences";
