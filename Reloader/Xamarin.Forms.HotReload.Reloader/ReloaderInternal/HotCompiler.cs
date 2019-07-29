73d4fd262c0e6a85a0929dd8bc3ebe58e5a715ae
Andrei Misiukevich <andrei.misiukevich@gmail.com>
2019-07-29 13:38:57 +0300

    android Mono compiler

--- Reloader/Xamarin.Forms.HotReload.Reloader/ReloaderInternal/HotCompiler.cs
+++ Reloader/Xamarin.Forms.HotReload.Reloader/ReloaderInternal/HotCompiler.cs
@@ -5,10 +5,11 @@ using System.Linq;
 using System.Reflection;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
+using Mono.CSharp;
 
 namespace Xamarin.Forms
 {
-    public interface IHotCompiler
+    internal interface IHotCompiler
     {
         Type Compile(string code, string className);
         bool TryLoadAssembly(Assembly assembly);
@@ -16,20 +17,24 @@ namespace Xamarin.Forms
 
     internal static class HotCompiler
     {
-        private static readonly Lazy<IHotCompiler> _lazyHotCompiler;
-
         static HotCompiler()
         {
-            _lazyHotCompiler = new Lazy<IHotCompiler>(() => new MicrosoftHotCompiler());
+            Current = Device.RuntimePlatform == Device.Android
+                    ? new MonoHotCompiler() as IHotCompiler
+                    : new MicrosoftHotCompiler();
             AppDomain.CurrentDomain.AssemblyLoad += (_, e) => Current.TryLoadAssembly(e.LoadedAssembly);
             foreach (var a in AppDomain.CurrentDomain.GetAssemblies())
             {
                 Current.TryLoadAssembly(a);
             }
-            Current.TryLoadAssembly(HotReloader.Current.App.GetType().Assembly);
+
+            if (!(Current is MonoHotCompiler))
+            {
+                Current.TryLoadAssembly(HotReloader.Current.App.GetType().Assembly);
+            }
         }
 
-        public static IHotCompiler Current => _lazyHotCompiler.Value;
+        public static IHotCompiler Current { get; }
 
         internal static bool? IsSupported { get; set; }
 
@@ -92,81 +97,82 @@ namespace Xamarin.Forms
                 return true;
             }
         }
-        #endregion
+		#endregion
 
-        //#region Mono
-        //private sealed class MonoHotCompiler : IHotCompiler
-        //{
-        //    private readonly Evaluator _evaluator;
-        //    private readonly ConsoleReportPrinter _reporter;
-        //    private readonly StringWriter _writer;
+		#region Mono
+		private sealed class MonoHotCompiler : IHotCompiler
+		{
+			private readonly Evaluator _evaluator;
+			private readonly ConsoleReportPrinter _reporter;
+			private readonly StringWriter _writer;
 
-        //    internal MonoHotCompiler()
-        //    {
-        //        _writer = new StringWriter();
-        //        _reporter = new ConsoleReportPrinter(_writer);
-        //        var context = new CompilerContext(new CompilerSettings
-        //        {
-        //            GenerateDebugInfo = false,
-        //            WarningsAreErrors = false,
-        //            LoadDefaultReferences = true,
-        //            Optimize = true,
-        //        }, _reporter);
-        //        _evaluator = new Evaluator(context);
-        //    }
+			internal MonoHotCompiler()
+			{
+				_writer = new StringWriter();
+				_reporter = new ConsoleReportPrinter(_writer);
+				var context = new CompilerContext(new CompilerSettings
+				{
+					GenerateDebugInfo = false,
+					WarningsAreErrors = false,
+					LoadDefaultReferences = true,
+					Optimize = true
+				}, _reporter);
+				_evaluator = new Evaluator(context);
+			}
 
-        //    public Type Compile(string code, string className)
-        //    {
-        //        //var oldClassName = className.Split('.').LastOrDefault();
-        //        //className = oldClassName + "GENERATED_POSTFIX" + Path.GetRandomFileName();
-        //        //code = code.Replace(oldClassName, className);
+			public Type Compile(string code, string className)
+			{
+                if (IsSupported == false)
+                {
+                    return null;
+                }
 
-        //        var writerStringBuilder = _writer.GetStringBuilder();
-        //        writerStringBuilder.Remove(0, writerStringBuilder.Length);
+                var writerStringBuilder = _writer.GetStringBuilder();
+				writerStringBuilder.Remove(0, writerStringBuilder.Length);
 
-        //        try
-        //        {
-        //            if (!_evaluator.Run(code) || _reporter.ErrorsCount > 0)
-        //            {
-        //                throw new Exception();
-        //            }
-        //            var getTypeMethod = _evaluator.Compile($"typeof({className})");
+				try
+				{
+					if (!_evaluator.Run(code) || _reporter.ErrorsCount > 0)
+					{
+						throw new Exception();
+					}
+					var getTypeMethod = _evaluator.Compile($"typeof({className})");
 
-        //            if (getTypeMethod == null)
-        //            {
-        //                throw new Exception();
-        //            }
+					if (getTypeMethod == null)
+					{
+						throw new Exception();
+					}
 
-        //            object typeObject = null;
-        //            getTypeMethod(ref typeObject);
-        //            var type = typeObject as Type;
-        //            return type;
-        //        }
-        //        catch
-        //        {
-        //            Console.WriteLine($"HOTRELOADER ERROR: {_writer.ToString()}");
-        //            return null;
-        //        }
-        //    }
+					object typeObject = null;
+					getTypeMethod(ref typeObject);
+					var type = typeObject as Type;
+					return type;
+				}
+				catch
+				{
+					Console.WriteLine($"HOTRELOADER ERROR: {_writer.ToString()}");
+					return null;
+				}
+			}
 
-        //    public bool TryLoadAssembly(Assembly assembly)
-        //    {
-        //        var name = assembly.GetName().Name;
-        //        if (name == "mscorlib" ||
-        //            name == "System" ||
-        //            name == "System.Core" ||
-        //            name.StartsWith("Mono.CSharp", StringComparison.CurrentCultureIgnoreCase) ||
-        //            name.StartsWith("Microsoft.CodeAnalysis", StringComparison.CurrentCultureIgnoreCase) ||
-        //            name.StartsWith("HotReload.HotCompile", StringComparison.CurrentCultureIgnoreCase) ||
-        //            name.StartsWith("eval-", StringComparison.CurrentCultureIgnoreCase))
-        //        {
-        //            return false;
-        //        }
+			public bool TryLoadAssembly(Assembly assembly)
+			{
+				var name = assembly.GetName().Name;
+				if (name == "mscorlib" ||
+					name == "System" ||
+					name == "System.Core" ||
+					name.StartsWith("Mono.CSharp", StringComparison.CurrentCultureIgnoreCase) ||
+					name.StartsWith("Microsoft.CodeAnalysis", StringComparison.CurrentCultureIgnoreCase) ||
+					name.StartsWith("HotReload.HotCompile", StringComparison.CurrentCultureIgnoreCase) ||
+					name.StartsWith("eval-", StringComparison.CurrentCultureIgnoreCase))
+				{
+					return false;
+				}
 
-        //        _evaluator?.ReferenceAssembly(assembly);
-        //        return true;
-        //    }
-        //}
-        //#endregion
-    }
+				_evaluator?.ReferenceAssembly(assembly);
+				return true;
+			}
+		}
+		#endregion
+	}
 }
